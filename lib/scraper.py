f9a1b235e0958dedfb07c02f4863bfebcd1e0e51
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-06-21 15:33:23 +0300

    Beautiful-soap-Scraping

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,4 +3,14 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+html = requests.get("https://Moringaschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+courses = doc.select('.heading-60-black.color-black.mb-20')
+
+for course in courses:
+    print(course.contents[0].strip())
+    
+# print(doc.select('.heading-60-black.color-black.mb-20'))
+
